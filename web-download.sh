38b346d58cf1146e066464823cdde47840a16905
Eugene Dorfman <eugene.dorfman@gmail.com>
2018-09-07 16:41:45 +0200

    fixing links

--- web-download.sh
+++ web-download.sh
@@ -1,9 +1,20 @@
+# delete old stuff
 rm -r www.pointfree.co
-
-wget -c --recursive -l 1 --load-cookies pointfree-cookies.txt https://www.pointfree.co/
-
 rm pointfree.zip
 
+# download new stuff
+wget -c --recursive -l 1 --load-cookies pointfree-cookies.txt https://www.pointfree.co/
+
+# update the links
+cd www.pointfree.co
+for f in `ls episodes/*`; do mv $f $f.html; done
+cat index.html | sed 's!/episodes/\([^"]*\)!episodes/\1.html!g' > index1.html
+mv index1.html index.html
+cd -
+
+# zip 
 zip -r pointfree.zip  www.pointfree.co
 
+# upload
 ../google-drive-wrapper/upload.sh pointfree.zip PointFree
+
