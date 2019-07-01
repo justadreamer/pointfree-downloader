e010550b0a161168d00b2881fc2c1181dd39df94
Eugene Dorfman <eugene.dorfman@gmail.com>
2019-07-01 20:26:57 +0200

    deleted glue.sh script

--- glue.sh
+++
@@ -1,4 +0,0 @@
-CHUNKS_DIR="content"
-OUTPUT_DIR="videos"
-mkdir $OUTPUT_DIR
-for episode in `ls $CHUNKS_DIR`; do echo "gluing $episode"; cat $CHUNKS_DIR/$episode/*.ts > $OUTPUT_DIR/$episode.m2ts; done
