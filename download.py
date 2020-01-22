74f6b40a38b8af37751b749f67dfcfc22e48b374
Eugene Dorfman <eugene.dorfman@gmail.com>
2020-01-22 12:00:01 +0200

    refactorings

--- download.py
+++ download.py
@@ -9,12 +9,10 @@ import sys
 from GoogleDriveWrapper import Folder
 from cookies import loadCookies
 from pathlib import PurePath
-import shutil
 
-CHUNKS_DIR = 'content'
+BASE_URL = 'https://www.pointfree.co/'
 VIDEOS_DIR = 'videos'
-
-requests.debug = True
+GDRIVE_PATH = 'Screencasts/PointFree'
 
 def appendPathComponent(base,addition):
     if not base[len(base)-1] == '/':
@@ -24,79 +22,34 @@ def appendPathComponent(base,addition):
     base+=addition
     return base
 
-#headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
-
 def downloadTextContent(url, cookies):
-    print("downloading text content from "+url)
     resp = requests.get(url, cookies=cookies)
     return resp.text
 
-def downloadContent(url, path, cookies=None):
-    print("downloading "+url+" to "+path)
-    resp = requests.get(url, stream=True, cookies=cookies)
-    if os.path.exists(path) and int(resp.headers['Content-Length']) == os.stat(path).st_size:
-        print("chunk "+path+" already downloaded, skipping")
-    else:
-        file = open(path, "wb")
-        file.write(resp.content)
-        file.close()
-
 class Episode:
     def __init__(self,baseURL,relativeURL):
+        # set from outside:
         self.baseURL = baseURL
         self.relativeURL = relativeURL
-        self.fullName = self.getFullName()
-        self.shortName = self.getShortName()
-        self.ext = 'm2ts'
-        self.gdriveUpload = False
-        self.leaveEmptyFile = False
+        self.ext = 'mp4'
+        self.gdriveUpload = True # by default it is true
+        self.removeLocal = False
+        self.forceReload = False
+        self.videoDir = VIDEOS_DIR
+
+        # computed and cached:
+        self.name = self.getEpisodeName()
+        self.fileName = self.getFileName()
 
     def __str__(self):
-        return "Episode: " + self.fullName
+        return "Episode: " + self.name
 
     def __repr__(self):
         return self.__str__()
 
-    def getFullName(self):
+    def getEpisodeName(self):
         components = self.relativeURL.split('/')
         return components[len(components)-1]
-
-    def getShortName(self):
-        components = self.fullName.split('-')
-        return components[0]
-
-    def makeEpisodePageURL(self):
-        pageURL = appendPathComponent(self.baseURL,self.relativeURL)
-        return pageURL
-
-    def getFilesFromM3U(self,m3uContent):
-        lines = m3uContent.split("\n")
-        filenames = list(filter(lambda l: len(l) > 0 and not l[0] == '#', lines))
-        return filenames
-
-    def getChunksSourceURL(self,markup):
-        soup = BeautifulSoup(markup, "html.parser")
-        print(soup)
-        video = soup.find("video")
-        source = video.find("source")
-        sourceURL = source["src"]
-        return sourceURL
-
-    def getChunksDir(self):
-        return os.path.join(os.getcwd(), CHUNKS_DIR, self.shortName)
-
-    def downloadTsFiles(self, baseChunksSourceURL, tsFiles, cookies=None):
-        if len(tsFiles) < 20:
-            print("skipping, probably cookie has expired")
-            return
-
-        dir = self.getChunksDir()
-        os.system('mkdir -p ' + dir)
-
-        for tsFile in tsFiles:
-            tsFileURL = appendPathComponent(baseChunksSourceURL, tsFile)
-            tsFilePath = os.path.join(dir, tsFile)
-            downloadContent(tsFileURL, tsFilePath, cookies=cookies)
 
     def getBaseURL(self,url):
         lastslashpos = url.rfind('/')
@@ -104,104 +57,76 @@ class Episode:
             lastsplashpos = url.rfind('/',0,len(url)-2)
         return url[0:lastslashpos]
 
-    def downloadChunks(self, cookies):
-        pageURL = self.makeEpisodePageURL()
-        pageContent = downloadTextContent(pageURL, cookies=cookies)
-        m3uURL1 = self.getChunksSourceURL(pageContent)
-        m3uContent1 = downloadTextContent(m3uURL1, cookies=cookies)
-        m3uFiles = self.getFilesFromM3U(m3uContent1)
-        m3uURLBase = self.getBaseURL(m3uURL1)
-
-        highQualityM3UFile = m3uFiles[0]
-        for m3uFile in m3uFiles:
-            if "high" in m3uFile:
-                highQualityM3UFile = m3uFile
-                break
-
-        m3uURL = os.path.join(m3uURLBase, highQualityM3UFile)
-        m3uContent2 = downloadTextContent(m3uURL, cookies=cookies)
-        tsFiles = self.getFilesFromM3U(m3uContent2)
-        print("will download the following chunks: ", tsFiles)
-        print("# of chunks: ", len(tsFiles))
-        self.downloadTsFiles(m3uURLBase, tsFiles)
-
-    def glueChunks(self):
-        videoDir = self.getVideoDir()
-        chunksDir = self.getChunksDir()
-        os.system('mkdir -p ' + videoDir)
-        if os.path.exists(chunksDir) and len(os.listdir(chunksDir)) > 0:
-            os.system('cat ' + chunksDir + '/*.ts > ' + self.getVideoFilePath())
-
-    def getFileName(self, name):
-        return name + '.' + self.ext
+    def getFileName(self):
+        return self.name + '.' + self.ext
 
     def getVideoDir(self):
-        return os.path.join(os.getcwd(), VIDEOS_DIR)
+        videoDir = os.path.expanduser(self.videoDir)
+        if os.path.isabs(videoDir):
+            return videoDir
+        return os.path.join(os.getcwd(), videoDir)
 
     def getVideoFilePath(self):
-        fullFileName = self.getFileName(self.fullName)
+        fullFileName = self.getFileName()
         fullFileName = os.path.join(self.getVideoDir(), fullFileName)
         return fullFileName
-
-    def renameExistingIfNeeded(self):
-        fullFileName = self.getFileName(self.fullName)
-        fullFilePath = self.getVideoFilePath()
-        if os.path.exists(fullFilePath):
-            return
-        shortFileName = self.getFileName(self.shortName)
-        shortFilePath = os.path.join(os.getcwd(), VIDEOS_DIR, shortFileName)
-        if os.path.exists(shortFilePath):
-            print("renaming " + shortFileName + " to " + fullFileName)
-            os.rename(shortFilePath,fullFilePath)
 
     def isDownloaded(self):
         return os.path.exists(self.getVideoFilePath())
 
     def gdriveUploadIfNeeded(self):
         if self.gdriveUpload:
-            folder = Folder(PurePath('Screencasts/PointFree'))
-            file = folder.fileForName(self.fullName)
-            if file == None:
-                print('uploading to google drive')
+            if not self.isGdriveAlreadyUploaded():
+                folder = Folder(PurePath(GDRIVE_PATH))
+                print('Uploading to gdrive')
                 folder.upload(self.getVideoFilePath())
             else:
-                print('already uploaded')
+                print('Already uploaded to gdrive')
 
-    def leaveEmptyFileIfNeeded(self):
-        if self.leaveEmptyFile:
-            filepath = self.getVideoFilePath()
-            os.remove(filepath)
-            fd = os.open(filepath, flags=os.O_CREAT, mode=0o644)
-            os.close(fd)
+    def removeLocalFileIfNeeded(self):
+        if self.removeLocal:
+            print('removing '+self.getVideoFilePath())
+            os.remove(self.getVideoFilePath())
 
-    def deleteChunks(self):
-        chunksDir = self.getChunksDir()
-        shutil.rmtree(chunksDir)
+    def isGdriveAlreadyUploaded(self):
+        folder = Folder(PurePath(GDRIVE_PATH))
+        file = folder.fileForName(self.getFileName())
+        return file is not None
 
-    def download(self, cookies):
-        print("Downloading", self)
-        self.renameExistingIfNeeded()
-        if self.isDownloaded():
-            print(self.fullName + ' is already downloaded')
+    # this method downloads the episode to the local folder, then uploads to Google Drive,
+    # unless something else is specified
+    def grab(self, cookies):
+        print("Grabbing", self)
+        url = self.makeEpisodeVideoURL(cookies)
+
+        if self.isDownloaded() and (not self.forceReload):
+            print(self.name + ' is already downloaded')
         else:
-            self.downloadChunks(cookies)
-            self.glueChunks()
+            if self.gdriveUpload and self.isGdriveAlreadyUploaded() and (not self.forceReload):
+                print(self.name + ' has already been uploaded to GDrive')
+            else:
+                command = self.downloadCommand(url)
+                os.system(command)
+
         self.gdriveUploadIfNeeded()
-        self.leaveEmptyFileIfNeeded() # so that we have a mark that we downloaded it
-        self.deleteChunks()
+        self.removeLocalFileIfNeeded()
 
-def saveUTF8Text(text,path):
-    file = open(path, "wb")
-    file.write(text.encode("utf-8"))
-    file.close()
+    # this is an overridable method
+    def makeEpisodeVideoURL(self, cookies):
+        pageURL = appendPathComponent(self.baseURL,self.relativeURL)
+        markup = downloadTextContent(pageURL,cookies)
+        soup = BeautifulSoup(markup, "html.parser")
+        iframe = soup.find("iframe")
+        sourceURL = iframe["src"]
+        return sourceURL
 
-def loadUTF8Text(path):
-    file = open(path,"rb")
-    content = file.read()
-    file.close()
-    return content.decode("utf-8")
+    # this is an overridable method
+    def downloadCommand(self, url):
+        command = 'youtube-dl --no-check-certificate --add-header "Referer:https://www.pointfree.co/"  --output ' + self.getVideoFilePath() + ' ' + url
+        return command
 
-def parseEpisodes(baseURL, cookies):
+def parseEpisodesPointFree(cookies):
+    baseURL = BASE_URL
     episodesPage = downloadTextContent(baseURL,cookies)
     soup = BeautifulSoup(episodesPage, "html.parser")
     episodes = []
@@ -215,39 +140,38 @@ def parseEpisodes(baseURL, cookies):
     return episodes
 
 def main():
-    baseURL = "https://www.pointfree.co/"
     cookieFileName = os.path.join(os.getcwd(), 'cookies.txt')
     cookies = loadCookies(cookieFileName)
-    episodes = parseEpisodes(baseURL, cookies)
+    episodes = parseEpisodesPointFree(cookies)
 
     if episodes is None or len(episodes) == 0:
         print("Error parsing episodes, check your cookies")
         return
 
 # TODO: use some nice argument parsing lib, like getopt
-    if '--gdrive-upload' in sys.argv:
-        for episode in episodes:
-            episode.gdriveUpload = True
-
-    if '--leave-empty-file' in sys.argv:
-        for episode in episodes:
-            episode.leaveEmptyFile = True
-
+    # now we form the list of episodes to attempt to grab:
     if '--last' in sys.argv or '--latest' in sys.argv:
         print("Downloading last episode only")
-        episodes[0].download(cookies)
+        episodes = episodes[:1]  # only the most recent one
     elif '-e' in sys.argv:
         argInd = sys.argv.index('-e')
         ep = sys.argv[argInd+1]
         print("Downloading episode",ep)
         for episode in episodes:
-            if ep in episode.fullName:
-                episode.download(cookies)
+            if ep in episode.name:
+                episodes = [episode]  # this single one to grab
                 break
-    else:
-        for episode in episodes:
-            episode.download(cookies)
 
+    gdriveUpload = not ('--no-gdrive-upload' in sys.argv)  # by default we upload to gdrive
+    removeLocal = '--remove-local' in sys.argv  # by default we leave the local files as is
+    forceReload = '--force-reload' in sys.argv  # by default we don't force reloading, if the file is already uploaded to gdrive f.e.
+
+    # we formed the list of episodes to grab, now we specify the settings and grab them
+    for episode in episodes:
+        episode.gdriveUpload = gdriveUpload
+        episode.removeLocal = removeLocal
+        episode.forceReload = forceReload
+        episode.grab(cookies)
 
 if __name__ == "__main__":
     main()
--- test_episode.py
+++ test_episode.py
@@ -1,26 +1,40 @@
 from unittest import TestCase
 from download import *
 
-
 class TestEpisode(TestCase):
     def setUp(self):
+        baseURL = 'http://base.com/'
         self.episode = Episode(baseURL, '/episodes/S01E01-good')
 
     def test_fullName(self):
-        self.assertEqual(self.episode.fullName, 'S01E01-good')
+        self.assertEqual(self.episode.name, 'S01E01-good')
 
     def test_shortName(self):
         self.assertEqual(self.episode.shortName, 'S01E01')
 
     def test_getFullFileName(self):
         shortFilename = self.episode.getFileName(self.episode.shortName)
-        self.assertEqual(shortFilename, 'S01E01.m2ts')
-        fullFilename = self.episode.getFileName(self.episode.fullName)
-        self.assertEqual(fullFilename, 'S01E01-good.m2ts')
+        self.assertEqual('S01E01.mp4', shortFilename)
+        fullFilename = self.episode.getFileName(self.episode.name)
+        self.assertEqual('S01E01-good.mp4', fullFilename)
 
     def test_getFullFilePath(self):
         fullFilePath = self.episode.getVideoFilePath()
-        self.assertEqual(fullFilePath, os.path.join(os.getcwd(),VIDEOS_DIR,'S01E01-good.m2ts'))
+        expected = os.path.join(os.getcwd(),VIDEOS_DIR,'S01E01-good.mp4')
+        self.assertEqual(expected, fullFilePath)
 
-    def test_getChunksDir(self):
-        self.assertEqual(self.episode.getChunksDir(),os.path.join(os.getcwd(),CHUNKS_DIR,'S01E01-good'))
+    def test_getVideoDir(self):
+        dir = '~/Movies' # absolute path but with a user home dir
+        self.episode.videoDir = dir
+        expected = os.path.expanduser(dir)
+        self.assertEqual(expected, self.episode.getVideoDir())
+
+        dir = '/Library/Movies' # absolute path
+        self.episode.videoDir = dir
+        expected = dir
+        self.assertEqual(expected, self.episode.getVideoDir())
+
+        dir = 'relative' #relative path
+        self.episode.videoDir = dir
+        expected = os.path.abspath(dir)
+        self.assertEqual(expected, self.episode.getVideoDir())
